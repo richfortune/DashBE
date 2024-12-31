bcddc1c0f0923a95a2fa43737024e9456c41b96b
richfortune <christian.agresta@xgmail.com>
2024-12-31 18:45:51 +0100

    Configurazione iniziale finita

---
+++ DashBe/DashBe.Domain/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using DashBe.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashBe.Domain.Models
+{
+    public class User : Entity<Guid>
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
---
+++ DashBe/DashBe.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DashBe.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashBe.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
